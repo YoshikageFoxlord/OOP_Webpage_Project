911f7361ce7e62280ae6101f57157656d7fe02c6
YoshikageFoxlord <113316622+YoshikageFoxlord@users.noreply.github.com>
2024-01-24 23:36:53 -0500

    Made the names appear on different lines.

--- src/Index.py
+++ src/Index.py
@@ -2,9 +2,9 @@ import tornado.web
 
 class Handler(tornado.web.RequestHandler):
     def get(self):
-        self.write('<a href="profile/alice">Alice Smith\n</a>')
-        self.write('<a href="profile/bob">Bob Jones\n</a>')
-        self.write('<a href="profile/carol">Carol Ling\n</a>')
-        self.write('<a href="profile/dave">Dave N. Port\n</a>')
+        self.write('<a href="profile/alice">Alice Smith</a><br>')
+        self.write('<a href="profile/bob">Bob Jones</a><br>')
+        self.write('<a href="profile/carol">Carol Ling</a><br>')
+        self.write('<a href="profile/dave">Dave N. Port</a><br>')
 
 
